0adcd7af76089bf47430d351caecb99062d73e3a
ajitjain <ajit.infotech@gmail.com>
2013-01-30 18:53:53 -0600

    prepare response for version no.

--- bigSemanticsService/src/ecologylab/semantic/service/mmdrepository/MMDRepositoryVersion.java
+++ bigSemanticsService/src/ecologylab/semantic/service/mmdrepository/MMDRepositoryVersion.java
@@ -3,6 +3,8 @@ package ecologylab.semantic.service.mmdrepository;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -23,8 +25,9 @@ public class MMDRepositoryVersion {
 
 	@GET
 	@Produces("text/plain")
-	public float getMmdRepositoryVersion()
+	public Response getMmdRepositoryVersion()
 	{
-		return SemanticsAssetVersions.METAMETADATA_ASSET_VERSION;
+		return Response.status(Status.OK).
+				entity((new Float(SemanticsAssetVersions.METAMETADATA_ASSET_VERSION).toString())).build();
 	}
 }
