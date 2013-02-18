998d70138c3340390582f2326dd9f7dc1658fc49
ajitjain <ajit.infotech@gmail.com>
2013-02-18 16:12:11 -0600

    correction in response code comment

--- BigSemanticsService/src/ecologylab/bigsemantics/service/SemanticServiceErrorCodes.java
+++ BigSemanticsService/src/ecologylab/bigsemantics/service/SemanticServiceErrorCodes.java
@@ -15,9 +15,9 @@ public interface SemanticServiceErrorCodes
 	//for error code 400
 	public final String BAD_REQUEST 	 = "Invalid query parameter";
 	
-	//404
-	public final String METADATA_NOT_FOUND = "Metadata service is currently unable to handle the request. Try again later.";
 	//503
+	public final String METADATA_NOT_FOUND = "Metadata service is currently unable to handle the request. Try again later.";
+	//404
 	public final String METAMETADATA_NOT_FOUND = "Meta-Metadata Not Found";
 	
 	//500
