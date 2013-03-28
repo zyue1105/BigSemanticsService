9ee4676f9b33bc2394cea5641ece4ba1587508e1
ajitjain <ajit.infotech@gmail.com>
2013-03-28 18:34:08 -0500

    intermediate commit

--- ServiceAnalytics/src/ecologylab/bigsemantics/service/utils/MacheUrlExtractor.java
+++ ServiceAnalytics/src/ecologylab/bigsemantics/service/utils/MacheUrlExtractor.java
@@ -5,28 +5,23 @@ package ecologylab.bigsemantics.service.utils;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
-import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonToken;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import ecologylab.bigsemantics.filestorage.SHA256FileNameGenerator;
-import ecologylab.bigsemantics.service.logging.ServiceLogRecord;
 import ecologylab.net.ParsedURL;
-import ecologylab.serialization.SIMPLTranslationException;
-import ecologylab.serialization.formatenums.StringFormat;
 
 /**
- * utility class to extract clipping source urls from the maches maches can be added one-by-one,
+ * utility class to extract clipping source urls from the maches. maches can be added one-by-one,
  * through a list, or a file.
  * 
  * @author ajit
@@ -35,13 +30,15 @@ import ecologylab.serialization.formatenums.StringFormat;
 
 public class MacheUrlExtractor
 {
-	ArrayList<String>	macheUrls;
+	ArrayList<String>	macheUrls						= new ArrayList<String>();
 
-	ArrayList<String>	clippingSourceUrls;
+	ArrayList<String>	clippingSourceUrls	= new ArrayList<String>();																																												;
 
-	OutputStream			outputStream	= System.out;
+	OutputStream			outputStream				= System.out;
 
-	static String			userAgent			= "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17";
+	static String			userAgent						= "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17";
+
+	boolean						bExtracted;
 
 	MacheUrlExtractor(String macheUrl, OutputStream outputStream)
 	{
@@ -86,14 +83,16 @@ public class MacheUrlExtractor
 
 	List<String> getMacheSourceUrls()
 	{
+		bExtracted = true;
 		if (macheUrls != null)
 		{
 			for (String macheUrl : macheUrls)
 			{
 				// get the mache metadata into a local file
-				String fn	= "/tmp/cache/" + SHA256FileNameGenerator.getName(ParsedURL.getAbsolute(macheUrl)) + ".html";
+				String fn = "/tmp/cache/"
+						+ SHA256FileNameGenerator.getName(ParsedURL.getAbsolute(macheUrl)) + ".html";
 				ProcessBuilder pb = new ProcessBuilder();
-				pb.command("/usr/local/bin/wget", "-O", fn, "-U", userAgent, macheUrl);
+				pb.command("/usr/bin/wget", "-O", fn, "-U", userAgent, macheUrl);
 				Process p = null;
 				try
 				{
@@ -106,8 +105,26 @@ public class MacheUrlExtractor
 						{
 							// map json response to get clipping source_doc
 							ObjectMapper m = new ObjectMapper();
-							JsonNode rootNode = m.readTree(f);
-							
+							JsonNode rootNode = m.readValue(f, JsonNode.class);
+
+							LinkedList<JsonNode> list = new LinkedList<JsonNode>();
+							list.add(rootNode);
+
+							JsonNode currentNode;
+							while ((currentNode = list.poll()) != null)
+							{
+								Iterator<JsonNode> it = currentNode.getElements();
+								while (it.hasNext())
+								{
+									JsonNode node = it.next();
+									String nodeName = node.toString();
+									if (nodeName.equals("source_doc")) // || nodeName.equals("outlinks")
+									{
+										clippingSourceUrls.add(node.getTextValue());
+									}
+								}
+							}
+
 							f.delete();
 						}
 					}
@@ -122,17 +139,34 @@ public class MacheUrlExtractor
 				}
 			}
 		}
-		return null;
+		return clippingSourceUrls;
 	}
 
 	void outputMacheSourceUrls()
 	{
+		if (!bExtracted)
+			getMacheSourceUrls();
+
 		if (clippingSourceUrls != null)
 		{
 			PrintWriter pw = new PrintWriter(outputStream);
 			for (String clippingSourceUrl : clippingSourceUrls)
 				pw.println(clippingSourceUrl);
 			pw.close();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			MacheUrlExtractor m = new MacheUrlExtractor(new File(
+					"/home/ajit/ServiceTest/urls/macheURLs.lst"));
+			m.outputMacheSourceUrls();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
 		}
 	}
 }
